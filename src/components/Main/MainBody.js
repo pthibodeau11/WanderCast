6678bf5d7d6522f7b31b45925cd84c7d81a1c5d1
Patrick Thibodeau <patrick.thibodeau@gmail.com>
2019-11-10 18:47:39 -0600

    added logos

--- src/components/Main/MainBody.js
+++ src/components/Main/MainBody.js
@@ -3,6 +3,12 @@ import "./MainBody.css";
 import location from "../../icons and logos/location.png";
 import personal from "../../icons and logos/personal.png";
 import quality from "../../icons and logos/quality.png";
+import ibm from "../../icons and logos/ibm.png";
+import zendesk from "../../icons and logos/zendesk.png";
+import google from "../../icons and logos/google.png";
+import stripe from "../../icons and logos/stripe.png";
+import nodemailer from "../../icons and logos/nodemailer.png";
+import firebase from "../../icons and logos/firebase.png";
 
 export default class MainBody extends Component {
   render() {
@@ -30,12 +36,15 @@ export default class MainBody extends Component {
             </p>
           </div>
         </div>
+        <h4 className="Powered-by">Powered by:</h4>
         <div className="Partner-logos">
-          <h1>Logo #1</h1>
-          <h1>Logo #2</h1>
-          <h1>Logo #3</h1>
-          <h1>Logo #4</h1>
-          <h1>Logo #5</h1>
+          <div className="Partner-logos-list">
+            <img src={firebase} alt="firebase" />
+            <img src={google} alt="google" />
+            <img src={ibm} alt="ibm" />
+            <img src={firebase} alt="firebase" />
+            <img src={nodemailer} alt="nodemailer" />
+          </div>
         </div>
       </>
     );

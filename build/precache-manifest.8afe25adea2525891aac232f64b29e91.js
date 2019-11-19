49cb1d9acefed3a5e02272ca880c1ae782d38365
Patrick Thibodeau <patrick.thibodeau@gmail.com>
2019-11-19 15:56:59 -0600

    4pm push

---
+++ build/precache-manifest.8afe25adea2525891aac232f64b29e91.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8771a5b48ffded72475077c2bfa56e47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e0a9a9438111cc2f5e9",
+    "url": "/static/css/2.2c485f3d.chunk.css"
+  },
+  {
+    "revision": "f91a836b8c421726cd2c",
+    "url": "/static/css/main.3ff28f5e.chunk.css"
+  },
+  {
+    "revision": "5e0a9a9438111cc2f5e9",
+    "url": "/static/js/2.d477237c.chunk.js"
+  },
+  {
+    "revision": "f91a836b8c421726cd2c",
+    "url": "/static/js/main.52f1e5f8.chunk.js"
+  },
+  {
+    "revision": "b0cb07adec621076562d",
+    "url": "/static/js/runtime-main.8ba3c4ae.js"
+  },
+  {
+    "revision": "ff65925256eeb3ce666c14c5072d9456",
+    "url": "/static/media/WanderCast_logo.ff659252.png"
+  },
+  {
+    "revision": "2bd09a51d0a13ef933cd6075c69417c5",
+    "url": "/static/media/background_landing.2bd09a51.jpg"
+  },
+  {
+    "revision": "cc5bce0165d4211864d6312ac4e6f7b0",
+    "url": "/static/media/firebase.cc5bce01.png"
+  },
+  {
+    "revision": "dfaccc8e6b4d769fd16045c968f5bc4b",
+    "url": "/static/media/google.dfaccc8e.png"
+  },
+  {
+    "revision": "7a7fe5d533c5cdfc6e3c2908400a90af",
+    "url": "/static/media/ibm.7a7fe5d5.png"
+  },
+  {
+    "revision": "d86a3932b3b31ea3f0e37507b344ce3e",
+    "url": "/static/media/location.d86a3932.png"
+  },
+  {
+    "revision": "caf5bb676ed18bc4a2b2a74531dd956a",
+    "url": "/static/media/logo512_red.caf5bb67.png"
+  },
+  {
+    "revision": "ddbe87bd2354f7ab21bef1fd144b9da7",
+    "url": "/static/media/menu-button.ddbe87bd.png"
+  },
+  {
+    "revision": "4f335c90e2e126771b7adcfc3d920f6a",
+    "url": "/static/media/nodemailer.4f335c90.png"
+  },
+  {
+    "revision": "000916c9606a49cf7f760cc404d103fb",
+    "url": "/static/media/personal.000916c9.png"
+  },
+  {
+    "revision": "25548103c4b09db1cc86750d4404c9e4",
+    "url": "/static/media/plain_background_landing.25548103.jpg"
+  },
+  {
+    "revision": "573d3b20d0fabc9c107e5cedd1eb1c16",
+    "url": "/static/media/quality.573d3b20.png"
+  },
+  {
+    "revision": "86dd57be34fa1cd10c7f50297b7be023",
+    "url": "/static/media/stripe.86dd57be.png"
+  },
+  {
+    "revision": "4b2089e12809167b3054d8f2e8116036",
+    "url": "/static/media/zendesk.4b2089e1.png"
+  }
+]);
